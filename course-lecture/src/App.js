0fdf74d2464d0cda7fb4a7904be82fc2060e8d1e
Danil-Dovhopolyi <65538352+Danil-Dovhopolyi@users.noreply.github.com>
2022-03-31 22:31:45 +0300

    Implemented button changer background

--- course-lecture/src/App.js
+++ course-lecture/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tabs from './Tabs';
+import ThemeContext, { themes } from './ThemeContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themes}>
+      <Tabs />
+    </ThemeContext.Provider>
   );
 }
 
